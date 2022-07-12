706acbb4b9fd01bbb599d591cc16d8f013ec25ff
zariefmalek <zariefmalek@gmail.com>
2022-07-13 02:50:49 +0800

    add bg

--- index.php
+++ index.php
@@ -4,20 +4,37 @@
 
 <!DOCTYPE HTML>
 <html>
+   
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     </head>
-    <body>
+    <body background="img/bg.png" >
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <?php include "inc/header.php" ?>
         <h1 class="text-center py-3 py-x">Welcome to Student complaint system!</h1>
-        <p class="text-justify px-5 fs-5">
-            Where is the complaint? What is a complaint? Who is complaining? What is a student? What is a system? Who are we welcoming?
-            This system aims to answer all these questions and help make it easier for students who study at the university
-            to make a complaint directly to the university.
+        <p class="text-center px-5 fs-5">
+            This website created for a student and staff to make a complaint about accomodity.
+        </p>
+        <p class="text-center px-5 fs-5">
+            This website created by:
+        </p>
+        <p class="text-center px-5 fs-3">
+            Muhammad Adib Bin Adnan
+        </p>
+        <p class="text-center px-5 fs-3">
+            Mohamad Imran Bin Izhar
+        </p>
+        <p class="text-center px-5 fs-3">
+            Amirul Zarief Bin Malek Amir Azmin
+        </p>
+        <p class="text-center px-5 fs-3">
+            Muhammad Hafiz Bin Norizan
+        </p>
+        <p class="text-center px-5 fs-3">
+            Siti Saudah Binti Mohd Ghani
         </p>
     </body>
 </html>
